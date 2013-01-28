8811062be7f94be5498102d4ec0a30907d5ee1eb
evgen <evgen@funtoo>
2013-01-28 10:13:30 +0600

    draw boards

---
+++ drawboard.py
@@ -0,0 +1,61 @@
+from pygame.locals import *
+import pygame
+
+GRIDLINECOLOR = (250,250,250)
+
+WINDOWWIDTH = 710
+WINDOWHEIGHT = 350
+SPACESIZE = 30
+BOARDWIDTH = 10
+BOARDHEIGHT =10
+
+XMARGIN = 30 #int((WINDOWWIDTH - (BOARDWIDTH * SPACESIZE)) / 2)
+YMARGIN = 30 #int((WINDOWHEIGHT - (BOARDHEIGHT * SPACESIZE)) / 2)
+
+pygame.init()
+DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
+
+def drawBoard(pix):
+
+    for x in range(BOARDWIDTH + 1):
+
+        startx = (x * SPACESIZE) + XMARGIN
+        starty = YMARGIN
+        endx = (x * SPACESIZE) + XMARGIN
+        endy = YMARGIN + (BOARDHEIGHT * SPACESIZE)
+        pygame.draw.line(DISPLAYSURF, GRIDLINECOLOR,(startx+pix,starty),(endx+pix,endy))
+
+    for y in range(BOARDHEIGHT + 1):
+
+        startx = XMARGIN
+        starty = (y * SPACESIZE) + YMARGIN
+        endx = XMARGIN + (BOARDWIDTH * SPACESIZE)
+        endy = (y * SPACESIZE) + YMARGIN
+        pygame.draw.line(DISPLAYSURF, GRIDLINECOLOR, (startx+pix, starty),(endx+pix,endy))
+
+
+
+def drawNotOwnBoaard(pix):
+
+    for x in range(BOARDWIDTH + 1):
+
+        startx = (x * SPACESIZE) + (XMARGIN+350)
+        starty = YMARGIN
+        endx = (x * SPACESIZE) + (XMARGIN+350)
+        endy = YMARGIN + (BOARDHEIGHT * SPACESIZE)
+        pygame.draw.line(DISPLAYSURF, GRIDLINECOLOR,(startx,starty),(endx,endy))
+
+    for y in range(BOARDHEIGHT + 1):
+
+        startx = XMARGIN+350
+        starty = (y * SPACESIZE) + (YMARGIN+0)
+        endx = (XMARGIN+350) + (BOARDWIDTH * SPACESIZE)
+        endy = (y * SPACESIZE) + (YMARGIN+0)
+        pygame.draw.line(DISPLAYSURF, GRIDLINECOLOR, (startx, starty),(endx,endy))
+
+
+while True:
+    drawBoard(0)
+    drawBoard(350)
+    pygame.display.update()
+
