e3fcd07ad7df1e7c6a9580c7467f067f21668a7f
evgen <evgen@funtoo>
2013-03-01 12:55:18 +0600

    move png files

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 from pygame.locals import *
 from time import sleep
-import view
-import model
-import controller
+import draw
+import ships
+import control
 import pygame
 import sys
 
-owncolor = view.OWNSHIPCOLOR
-enemycolor = view.ENEMYSHIPCOLOR
+owncolor = draw.OWNSHIPCOLOR
+enemycolor = draw.ENEMYSHIPCOLOR
 
-view.drawBoard()
-view.drawBoard(360)
+draw.drawBoard()
+draw.drawBoard(360)
 
-ownship = model.workShip()
-enemyship = model.workShip()
+ownship = ships.workShip()
+enemyship = ships.workShip()
 
 OwnListShip = ownship.createListShips()
 EnemyListShip = enemyship.createListShips()
 
-view.drawAllShip(ownship.korabli, owncolor, 'own')
-#view.drawAllShip(enemyship.korabli, enemycolor, 'enemy')
+draw.drawAllShip(ownship.korabli, owncolor, 'own')
+#draw.drawAllShip(enemyship.korabli, enemycolor, 'enemy')
 
 #for i in enemyship.korabli:
 #    print i.cells
@@ -32,7 +32,7 @@ while True:
             sys.exit()
         if event.type == MOUSEBUTTONUP:
             mousex, mousey = event.pos
-            controller.vistrel(ownship.korabli, enemyship, mousex, mousey)
+            control.vistrel(ownship.korabli, enemyship, mousex, mousey)
         pygame.display.update()
     sleep(0.05)
 
