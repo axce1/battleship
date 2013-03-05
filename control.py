ae60c4cf9e1080e6c35cac1fb613330b6744215f
evgen <evgen@funtoo>
2013-03-05 10:04:48 +0600

    working water and explosion

--- control.py
+++ control.py
@@ -3,13 +3,12 @@ import random
 import pygame
 from pygame.locals import *
 
+
 def vistrel(ownship, ship, mx, my):
     y = (mx-390)/30
     x = my/30 - 1
     ship.delFromKorabli(x,y)
-    for i in ship.korabli:
-        if (x,y) not in i.hitspace:
-            ship.bumspace.add((x,y))
+    bumbum(ship,x,y)
     draw.reDrawAll(ownship, ship, draw.DISPLAYSURF)
 
 
@@ -18,10 +17,19 @@ def compTurn(enemyship,ship):
     y = random.randint(0,9)
 
     ship.delFromKorabli(x,y)
-    for i in ship.korabli:
-        if (x,y) not in i.hitspace:
-            ship.bumSpace(x,y)
+    bumbum(ship,x,y)
     draw.reDrawAll(ship,enemyship,draw.DISPLAYSURF)
+
+
+def bumbum(ship,f,n):
+    allshipspace = set([])
+    for k in ship.korabli:
+        for (i,j) in k.hitspace:
+            allshipspace.add((i,j))
+        for (i,j) in k.cells:
+            allshipspace.add((i,j))
+    if (f,n) not in allshipspace:
+        ship.bumspace.add((f,n))
 
 
 def checkForKeyPress():
